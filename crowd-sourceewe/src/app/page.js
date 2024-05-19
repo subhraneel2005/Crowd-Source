6b46086b4ca07e010e69f0aea56fe27632bca645
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-05-19 20:20:03 +0530

    ui updated homepage #1

--- crowd-sourceewe/src/app/page.js
+++ crowd-sourceewe/src/app/page.js
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full bg-purple-800 text-purple-300">
       <nav className="top-0 left-0 w-full py-10 px-5 bg-black opacity-30">
-
       </nav>
+        <div className="w-full min-h-screen flex justify-center items-center">
+            <h1 className="text-center text-[7vw]">Welcome to Crowd Source</h1>
+        </div>
     </div>
   );
 }
