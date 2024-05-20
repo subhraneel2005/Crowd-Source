7ab7d7fa58f3572a63ff5d3e717d78e3e75cec5e
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-05-20 08:51:05 +0530

    landing Page done#1

--- crowd-sourceewe/src/app/page.js
+++ crowd-sourceewe/src/app/page.js
@@ -1,12 +1,24 @@
 import Image from "next/image";
+import textLogo from "/public/Text.png"
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-purple-800 text-purple-300">
-      <nav className="top-0 left-0 w-full py-10 px-5 bg-black opacity-30">
+    <div className="min-h-screen w-full text-purple-300">
+      <nav className="top-0 left-0 w-full py-6 px-5 flex justify-between">
+        <h1 className="md:text-3xl text-2xl font-bold text-sky-500">Crowd Source</h1>
+        <ul className="flex gap-3 md:gap-12">
+          <li className="text-xl font-bold hover:text-purple-400 duration-300 cursor-pointer">Home</li>
+          <li className="text-xl font-bold hover:text-purple-400 duration-300 cursor-pointer">Sign In</li>
+          <li className="text-xl font-bold hover:text-purple-400 duration-300 cursor-pointer">About Me</li>
+        </ul>
       </nav>
         <div className="w-full min-h-screen flex justify-center items-center">
-            <h1 className="text-center text-[7vw]">Welcome to Crowd Source</h1>
+          <div className="block space-y-6">
+            <h1 className="text-center font-semibold md:text-[6vw] text-5xl">Welcome to Crowd Source 💵</h1>
+            <div className="w-full h-full flex justify-center items-center">
+            <button class="btn btn-accent">Explore Communities 🌸</button>
+            </div>
+          </div>
         </div>
     </div>
   );
---
+++ crowd-sourceewe/src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAijafKHTIFbPs5hketzzwQrNDKr3kW3RU",
+  authDomain: "crowd-source-1e395.firebaseapp.com",
+  projectId: "crowd-source-1e395",
+  storageBucket: "crowd-source-1e395.appspot.com",
+  messagingSenderId: "228371351474",
+  appId: "1:228371351474:web:ae1a4fc3b2566936b206ab"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
